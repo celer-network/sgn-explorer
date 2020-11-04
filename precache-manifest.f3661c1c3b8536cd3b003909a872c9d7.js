4e7c17548ef9fef60f84d0e0e34406e27331187d
Michael Zhou <zhoumotongxue008@gmail.com>
2020-11-04 12:40:18 +0800

    [skip ci] Update ropsten deployment

---
+++ precache-manifest.f3661c1c3b8536cd3b003909a872c9d7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5055d0695fe903978870bb937dfa27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94ba891a0d43b712ebae",
+    "url": "/static/css/5.b6e3a137.chunk.css"
+  },
+  {
+    "revision": "6e4102b89e4049717d01",
+    "url": "/static/css/main.4f79e1df.chunk.css"
+  },
+  {
+    "revision": "9316332d9f9d30074a59",
+    "url": "/static/js/0.e77684a2.chunk.js"
+  },
+  {
+    "revision": "817aa803e0e51e4e8f42",
+    "url": "/static/js/1.7a6b8793.chunk.js"
+  },
+  {
+    "revision": "2b3314d957e7ac691cbf",
+    "url": "/static/js/2.dec140e1.chunk.js"
+  },
+  {
+    "revision": "94ba891a0d43b712ebae",
+    "url": "/static/js/5.f578c330.chunk.js"
+  },
+  {
+    "revision": "c1ec73a8c73a266fa4d17afd03d284b1",
+    "url": "/static/js/5.f578c330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97daf0f554925e7f9385",
+    "url": "/static/js/6.119cedbf.chunk.js"
+  },
+  {
+    "revision": "930d66a770bda03cdcc7",
+    "url": "/static/js/7.12363ffe.chunk.js"
+  },
+  {
+    "revision": "8af6d53e87d144700d02",
+    "url": "/static/js/8.e7d49a42.chunk.js"
+  },
+  {
+    "revision": "fa507523087d500b9185",
+    "url": "/static/js/9.073f1413.chunk.js"
+  },
+  {
+    "revision": "6e4102b89e4049717d01",
+    "url": "/static/js/main.fd1d4b88.chunk.js"
+  },
+  {
+    "revision": "b4fe22bc9cb70692c4ce",
+    "url": "/static/js/runtime-main.6b45b2ec.js"
+  }
+]);
