804735dc9630a2859308c63dd2541e62e71a7aa1
Michael Zhou <zhoumotongxue008@gmail.com>
2020-11-06 02:46:51 +0800

    [skip ci] Update ropsten deployment

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f3661c1c3b8536cd3b003909a872c9d7.js"
+  "/precache-manifest.3287983d340db6cf5329543c84e1af7c.js"
 );
 
 self.addEventListener('message', (event) => {
