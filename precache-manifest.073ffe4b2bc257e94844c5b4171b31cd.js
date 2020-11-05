2950e894a7710f58715ee4fcf31014a5de8cca7d
Xuefeng Zhu <xzhu15@illinois.edu>
2020-11-04 23:19:26 -0800

    [skip ci] Update ropsten deployment

---
+++ precache-manifest.073ffe4b2bc257e94844c5b4171b31cd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c035c8afb9e7fa2adbfc84e1102a00f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c83f0718232b74e70249",
+    "url": "/static/css/5.b6e3a137.chunk.css"
+  },
+  {
+    "revision": "111189d22ee8314e5c0a",
+    "url": "/static/css/main.4f79e1df.chunk.css"
+  },
+  {
+    "revision": "2be4a1dd6caa6fd001c6",
+    "url": "/static/js/0.9ccaee8e.chunk.js"
+  },
+  {
+    "revision": "c9783fff52b5f4595970",
+    "url": "/static/js/1.29b925ac.chunk.js"
+  },
+  {
+    "revision": "87bf73d2c570c59d1b12",
+    "url": "/static/js/2.e524cd3f.chunk.js"
+  },
+  {
+    "revision": "c83f0718232b74e70249",
+    "url": "/static/js/5.22eaa3fd.chunk.js"
+  },
+  {
+    "revision": "8b30c288f26213de572a72c2174138dc",
+    "url": "/static/js/5.22eaa3fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf89660fe4bd53c3faf",
+    "url": "/static/js/6.b98528e2.chunk.js"
+  },
+  {
+    "revision": "148685336cce63902e4b",
+    "url": "/static/js/7.40a20aa8.chunk.js"
+  },
+  {
+    "revision": "9625dffdf5acd39ef409",
+    "url": "/static/js/8.1f242892.chunk.js"
+  },
+  {
+    "revision": "a9c7688c3282715f60ee",
+    "url": "/static/js/9.10aa0b2a.chunk.js"
+  },
+  {
+    "revision": "111189d22ee8314e5c0a",
+    "url": "/static/js/main.77182c2f.chunk.js"
+  },
+  {
+    "revision": "71e67a8321df7ee7a19d",
+    "url": "/static/js/runtime-main.4d91a0f3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f3661c1c3b8536cd3b003909a872c9d7.js"
+  "/precache-manifest.073ffe4b2bc257e94844c5b4171b31cd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
