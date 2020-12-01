64248a9e46df711c2aa53cfe6b64e6cafbf51003
Xuefeng Zhu <xzhu15@illinois.edu>
2020-11-30 22:32:54 -0800

    Remove wei unit

--- src/components/candidate/delegator-table.js
+++ src/components/candidate/delegator-table.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { drizzleConnect } from '@drizzle/react-plugin';
-import { Statistic, Row, Col, message, Table } from 'antd';
+import { Row, Col, message, Table } from 'antd';
 import axios from 'axios';
 import web3 from 'web3';
 
